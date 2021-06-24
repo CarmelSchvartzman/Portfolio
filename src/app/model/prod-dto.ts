36e155fd6f9578ed03c2e1ded94ac8dbc36bec81
Carmel Shvartzman <Carmel.asnet@gmail.com>
2021-06-24 08:31:21 +0300

    1.0.0.2

--- src/app/model/prod-dto.ts
+++ src/app/model/prod-dto.ts
@@ -3,8 +3,12 @@ export interface ProdDto
   categoria: string;
   desc1: string;
   desc2: string;
+  img1: string;
+  img2: string;
+  img3: string;
   producto: string;
   resumen: string;
   subtitulo1: string;
   subtitulo2: string;
+  url: string;
 }
